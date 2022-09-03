6c8aa37cd928f51a4d4446271e2c1cd87fc44e83
davuboy <germanramirezrob@gmail.com>
2022-09-03 11:29:11 -0500

    FCC-projects SETUP DONE

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+//Hex values = #  ---> 0 to 9 and A to F
+
+btn.addEventListener("click", function(){
+    let hexColor = "#";
+    for(let i = 0; i < 6; i++){
+        hexColor += hex[getRandomNumber()];
+        
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
